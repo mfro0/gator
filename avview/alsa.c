eb81da951bac3dbaaefb9ce6bf070c9352cdf0cb
volodya <volodya>
2001-12-13 18:04:13 +0000

    Start of alsa support.

---
+++ avview/alsa.c
@@ -0,0 +1,81 @@
+/*     avview preliminary version
+
+       (C) Vladimir Dergachev 2001
+       
+       GNU Public License
+       
+*/
+
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <errno.h>
+
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <tcl.h>
+#include <tk.h>
+
+#include <alsa/asoundlib.h>
+
+#include "string_cache.h"
+#include "global.h"
+#include "alsa.h"
+
+int alsa_card_get_name(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
+{
+char *card_name;
+int a;
+Tcl_ResetResult(interp);
+if(argc<2){
+	Tcl_AppendResult(interp,"ERROR: alsa_card_get_name requires one argument", NULL);
+	return TCL_ERROR;
+	}
+if((a=snd_card_get_name(atoi(argv[1]), &card_name))<0){
+	Tcl_AppendResult(interp,"ERROR: alsa_card_get_name: ", NULL);
+	Tcl_AppendResult(interp, snd_strerror(a), NULL);
+	return TCL_ERROR;
+	}
+Tcl_AppendResult(interp, card_name, NULL);
+return 0;
+}
+
+int alsa_card_get_longname(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
+{
+char *card_longname;
+int a;
+Tcl_ResetResult(interp);
+if(argc<2){
+	Tcl_AppendResult(interp,"ERROR: alsa_card_get_longname requires one argument", NULL);
+	return TCL_ERROR;
+	}
+if((a=snd_card_get_longname(atoi(argv[1]), &card_longname))<0){
+	Tcl_AppendResult(interp,"ERROR: alsa_card_get_longname: ", NULL);
+	Tcl_AppendResult(interp, snd_strerror(a), NULL);
+	return TCL_ERROR;
+	}
+Tcl_AppendResult(interp, card_longname, NULL);
+return 0;
+}
+
+struct {
+	char *name;
+	Tcl_CmdProc *command;
+	} alsa_commands[]={
+	{"alsa_card_get_name", alsa_card_get_name},
+	{"alsa_card_get_longname", alsa_card_get_longname},
+	{NULL, NULL}
+	};
+
+void init_alsa(Tcl_Interp *interp)
+{
+long i;
+
+for(i=0;alsa_commands[i].name!=NULL;i++)
+	Tcl_CreateCommand(interp, alsa_commands[i].name, alsa_commands[i].command, (ClientData)0, NULL);
+}
---
+++ avview/alsa.h
@@ -0,0 +1,8 @@
+#ifndef __ALSA_H__
+#define __ALSA_H__
+
+
+void init_alsa(Tcl_Interp *interp);
+
+
+#endif
