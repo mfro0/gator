520c39369462cbcb067c083af554fdc86566e4b2
volodya <volodya>
2001-12-17 19:42:26 +0000

    More progress with alsa support.

--- avview/alsa.c
+++ avview/alsa.c
@@ -34,10 +34,11 @@ snd_pcm_info_t *pcminfo;
 snd_rawmidi_info_t *rawmidiinfo;
 
 typedef struct {
-	snd_ctl_t *ctl;
-	snd_ctl_card_info_t *info;
-	snd_ctl_elem_list_t *elist;
-	snd_ctl_elem_info_t *einfo;
+	snd_hctl_t *hctl;
+	long elem_count;
+	snd_hctl_elem_t **elem;
+	snd_ctl_elem_info_t **einfo;
+	snd_ctl_elem_type_t *etype;
 	} ALSA_DATA;
 
 #include "global.h"
@@ -70,7 +71,8 @@ if(argc<2){
 	Tcl_AppendResult(interp,"ERROR: alsa_card_next requires one argument", NULL);
 	return TCL_ERROR;
 	}
-if((a=snd_card_get_name(atoi(argv[1]), &next_card))<0){
+next_card=atoi(argv[1]);
+if((a=snd_card_next(&next_card))<0){
 	Tcl_AppendResult(interp,"ERROR: alsa_card_next: ", NULL);
 	Tcl_AppendResult(interp, snd_strerror(a), NULL);
 	return TCL_ERROR;
@@ -110,10 +112,11 @@ if((a=snd_card_load(atoi(argv[1])))<0){
 	Tcl_AppendResult(interp, snd_strerror(a), NULL);
 	return TCL_ERROR;
 	}
+Tcl_SetObjResult(interp, Tcl_NewIntObj(a));
 return 0;
 }
 
-int alsa_ctl_open(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
+int alsa_hctl_open(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
 {
 int a,i;
 int mode;
@@ -122,120 +125,208 @@ Tcl_Obj *ans;
 
 Tcl_ResetResult(interp);
 if(argc<3){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_open requires two arguments", NULL);
+	Tcl_AppendResult(interp,"ERROR: alsa_hctl_open requires two arguments", NULL);
 	return TCL_ERROR;
 	}
 i=add_string(alsa_sc, argv[1]);
 if(alsa_sc->data[i]!=NULL){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_open: this control is already open", NULL);
+	Tcl_AppendResult(interp,"ERROR: alsa_hctl_open: this control is already open", NULL);
 	return TCL_ERROR;
 	}
 alsa_sc->data[i]=do_alloc(1, sizeof(ALSA_DATA));
 mode=0;
 ad=(ALSA_DATA*)alsa_sc->data[i];
-if((a=snd_ctl_open(&(ad->ctl), argv[1], mode))<0){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_open: ", NULL);
+if((a=snd_hctl_open(&(ad->hctl), argv[1], mode))<0){
+	Tcl_AppendResult(interp,"ERROR: alsa_hctl_open: ", NULL);
 	Tcl_AppendResult(interp, snd_strerror(a), NULL);
 	free(alsa_sc->data[i]);
 	alsa_sc->data[i]=NULL;
 	return TCL_ERROR;
 	}
-snd_ctl_card_info_alloca(&(ad->info));
-if((a=snd_ctl_card_info(ad->ctl, ad->info))<0){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_open: ", NULL);
+if((a=snd_hctl_load(ad->hctl))<0){
+	Tcl_AppendResult(interp,"ERROR: alsa_hctl_open: ", NULL);
 	Tcl_AppendResult(interp, snd_strerror(a), NULL);
-	snd_ctl_close(ad->ctl);
-	free(ad->info);
+	snd_hctl_close(ad->hctl);
 	free(alsa_sc->data[i]);
 	alsa_sc->data[i]=NULL;
 	return TCL_ERROR;
 	}
-snd_ctl_elem_list_alloca(&(ad->elist));
-if((a=snd_ctl_elem_list(ad->ctl, ad->elist))<0){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_open: ", NULL);
-	Tcl_AppendResult(interp, snd_strerror(a), NULL);
-	snd_ctl_close(ad->ctl);
-	free(ad->elist);
-	free(ad->info);
-	free(alsa_sc->data[i]);
-	alsa_sc->data[i]=NULL;
+ad->elem=NULL;
+ad->einfo=NULL;
+ad->etype=NULL;
+ad->elem_count=-1;
+return TCL_OK;
+}
+
+int alsa_hctl_close(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
+{
+int a,i,j,k,items;
+int mode;
+ALSA_DATA *ad;
+Tcl_Obj *ans,*list,*list2;
+int count;
+snd_hctl_elem_t *elem;
+
+Tcl_ResetResult(interp);
+if(argc<2){
+	Tcl_AppendResult(interp,"ERROR: alsa_hctl_close requires one argument", NULL);
 	return TCL_ERROR;
 	}
-snd_ctl_elem_info_alloca(&(ad->einfo));
-if((a=snd_ctl_elem_list(ad->ctl, ad->einfo))<0){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_open: ", NULL);
-	Tcl_AppendResult(interp, snd_strerror(a), NULL);
+i=lookup_string(alsa_sc, argv[1]);
+if((i<0)||((ad=(ALSA_DATA *)alsa_sc->data[i])==NULL)){
+	return TCL_OK;
+	}
+if(ad->elem_count>0){
+	for(k=0;k<ad->elem_count;k++){
+		snd_ctl_elem_info_free(ad->einfo[j]);
+		}
 	free(ad->einfo);
-	snd_ctl_elem_list_free_space(ad->elist);
-	snd_ctl_close(ad->ctl);
-	free(ad->info);
-	free(alsa_sc->data[i]);
-	alsa_sc->data[i]=NULL;
+	free(ad->elem);
+	free(ad->etype);
+	ad->elem_count=0;
+	ad->einfo=NULL;
+	ad->etype=NULL;
+	}
+snd_hctl_close(ad->hctl);
+free(ad);
+alsa_sc->data[i]=NULL;
+return TCL_OK;
+}
+
+int alsa_hctl_get_elements_info(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
+{
+int a,i,j,k,items;
+int mode;
+ALSA_DATA *ad;
+Tcl_Obj *ans,*list,*list2;
+int count;
+snd_hctl_elem_t *elem;
+
+Tcl_ResetResult(interp);
+if(argc<2){
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_elements_info requires one argument", NULL);
 	return TCL_ERROR;
 	}
-
+i=lookup_string(alsa_sc, argv[1]);
+if((i<0)||((ad=(ALSA_DATA *)alsa_sc->data[i])==NULL)){
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_elements_info: no such control open", NULL);
+	return TCL_ERROR;
+	}
+count=snd_hctl_get_count(ad->hctl);
+ad->elem_count=count;
+ad->elem=do_alloc(count, sizeof(snd_hctl_elem_t *));
+ad->einfo=do_alloc(count, sizeof(snd_ctl_elem_info_t *));
+ad->etype=do_alloc(count, sizeof(snd_ctl_elem_type_t));
+j=0;
 ans=Tcl_NewListObj(0, NULL);
-/* ok, we can't simply count on the order of entries being fixed */
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-card", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewIntObj(snd_ctl_card_info_get_card(ad->info)));
+for(elem=snd_hctl_first_elem(ad->hctl);elem!=NULL;elem=snd_hctl_elem_next(elem)){
+	ad->elem[j]=elem;
+	snd_ctl_elem_info_malloc(&(ad->einfo[j]));
+        snd_hctl_elem_info(elem, ad->einfo[j]);
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-id", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_card_info_get_id(ad->info), -1));
+	list=Tcl_NewListObj(0, NULL);
+	/* ok, we can't simply count on the order of entries being fixed */
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-interface", -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj(snd_ctl_elem_iface_name(snd_hctl_elem_get_interface(elem)), -1));
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-driver", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_card_info_get_driver(ad->info), -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-name", -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj(snd_hctl_elem_get_name(elem), -1));
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-name", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_card_info_get_name(ad->info), -1));
+	ad->etype[j]=snd_ctl_elem_info_get_type(ad->einfo[j]);
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-longname", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_card_info_get_longname(ad->info), -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-type", -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj(snd_ctl_elem_type_name(ad->etype[j]), -1));
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-mixer", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_card_info_get_mixername(ad->info), -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-values_count", -1));
+	Tcl_ListObjAppendElement(interp, list, Tcl_NewIntObj(snd_ctl_elem_info_get_count(ad->einfo[j])));
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-components", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_card_info_get_components(ad->info), -1));
+	switch(ad->etype[j]){
+		case SND_CTL_ELEM_TYPE_INTEGER:
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-min", -1));
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewIntObj(snd_ctl_elem_info_get_min(ad->einfo[j])));
 
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-controls", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewIntObj(snd_ctl_elem_list_get_count(ad->elist)));
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-max", -1));
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewIntObj(snd_ctl_elem_info_get_max(ad->einfo[j])));
+
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-step", -1));
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewIntObj(snd_ctl_elem_info_get_step(ad->einfo[j])));
+			break;
+		case SND_CTL_ELEM_TYPE_ENUMERATED:
+			items=snd_ctl_elem_info_get_items(ad->einfo[j]);
+			Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj("-items", -1));
+			list2=Tcl_NewListObj(0, NULL);
+			for(k=0;k<items;k++){
+				snd_ctl_elem_info_set_item(ad->einfo[j], k);
+				Tcl_ListObjAppendElement(interp, list, Tcl_NewStringObj(snd_ctl_elem_info_get_item_name(ad->einfo[j]), -1));
+				}
+			Tcl_ListObjAppendElement(interp,list,list2);
+			break;
+		}
+	Tcl_ListObjAppendElement(interp, ans, list);
+	j++;
+	}
 
 Tcl_SetObjResult(interp, ans);
 return TCL_OK;
 }
 
-
-int alsa_ctl_get_element_info(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
+int alsa_hctl_get_element_value(ClientData client_data,Tcl_Interp* interp,int argc,char *argv[])
 {
-int a,i;
-int mode;
+long i,e;
+int a,k;
 ALSA_DATA *ad;
+snd_ctl_elem_value_t *value;
 Tcl_Obj *ans;
-snd_ctl_elem_id_t *elem_id;
-snd_ctl_elem_iface_t iface;
-snd_ctl_elem_type_t type;
-int index;
-
 Tcl_ResetResult(interp);
 if(argc<3){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_info requires two arguments", NULL);
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_value requires two arguments", NULL);
 	return TCL_ERROR;
 	}
 i=lookup_string(alsa_sc, argv[1]);
 if((i<0)||((ad=(ALSA_DATA *)alsa_sc->data[i])==NULL)){
-	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_info: no such control open", NULL);
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_value: no such control open", NULL);
 	return TCL_ERROR;
 	}
-index=atoi(argv[2]);
-snd_ctl_elem_id_alloca(&elem_id);
-/* snd_ctl_elem_list_get_id(ad->elist, index, elem_id); 
-  iface=snd_ctl_elem_id_get_interface(elem_id);
-*/
+if((ad->elem==NULL)||(ad->einfo==NULL)||(ad->etype==NULL)){
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_value: you must call alsa_ctl_get_elements_info first", NULL);
+	return TCL_ERROR;
+	}
+e=atoi(argv[2]);
+if((e<0)||(e>=ad->elem_count)){
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_value: no such element", NULL);
+	return TCL_ERROR;
+	}
+ 
+if((a=snd_ctl_elem_value_malloc(&value))<0){
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_value: ", NULL);
+	Tcl_AppendResult(interp, snd_strerror(a), NULL);
+	return TCL_ERROR;
+	}
+if((a=snd_hctl_elem_read(ad->elem[e], value))<0){
+	Tcl_AppendResult(interp,"ERROR: alsa_ctl_get_element_value: ", NULL);
+	Tcl_AppendResult(interp, snd_strerror(a), NULL);
+	free(value);
+	return TCL_ERROR;
+	}
 ans=Tcl_NewListObj(0, NULL);
-/* ok, we can't simply count on the order of entries being fixed */
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("-interface", -1));
-Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_elem_iface_name(snd_ctl_elem_list_get_interface(ad->elist, index)), -1));
-/* snd_ctl_elem_id_free(elem_id); */
+for(k=0;k<snd_ctl_elem_info_get_count(ad->einfo[e]);k++){
+	switch(ad->etype[e]){
+		case SND_CTL_ELEM_TYPE_INTEGER:
+			Tcl_ListObjAppendElement(interp, ans, Tcl_NewIntObj(snd_ctl_elem_value_get_integer(value,k)));
+			break;
+		case SND_CTL_ELEM_TYPE_BOOLEAN:
+			Tcl_ListObjAppendElement(interp, ans, Tcl_NewIntObj(snd_ctl_elem_value_get_boolean(value,k)));
+			break;
+		case SND_CTL_ELEM_TYPE_ENUMERATED:
+			snd_ctl_elem_info_set_item(ad->einfo[e], snd_ctl_elem_value_get_enumerated(value,k));
+			Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj(snd_ctl_elem_info_get_item_name(ad->einfo[e]),-1));			
+			break;
+		default:
+			Tcl_ListObjAppendElement(interp, ans, Tcl_NewStringObj("undecipherable",-1));			
+		}
+	}
+snd_ctl_elem_value_free(value);
+Tcl_SetObjResult(interp, ans);
 return TCL_OK;
 }
 
@@ -255,8 +346,10 @@ struct {
 	{"alsa_card_get_longname", alsa_card_get_longname},
 	{"alsa_card_next", alsa_card_next},
 	{"alsa_card_load", alsa_card_load},
-	{"alsa_ctl_open", alsa_ctl_open},
-	{"alsa_ctl_get_element_info", alsa_ctl_get_element_info},
+	{"alsa_hctl_open", alsa_hctl_open},
+	{"alsa_hctl_close", alsa_hctl_close},
+	{"alsa_hctl_get_elements_info", alsa_hctl_get_elements_info},
+	{"alsa_hctl_get_element_value", alsa_hctl_get_element_value},
 	{NULL, NULL}
 	};
 
@@ -266,8 +359,8 @@ long i;
 
 alsa_sc=new_string_cache();
 
-snd_pcm_info_alloca(&pcminfo);
-snd_rawmidi_info_alloca(&rawmidiinfo);
+snd_pcm_info_malloc(&pcminfo);
+snd_rawmidi_info_malloc(&rawmidiinfo);
 
 for(i=0;alsa_commands[i].name!=NULL;i++)
 	Tcl_CreateCommand(interp, alsa_commands[i].name, alsa_commands[i].command, (ClientData)0, NULL);
