35b095c275f9a35a5a1c59d734d93a70a2cb014b
esellers <esellers>
2006-10-21 01:16:10 +0000

    should work on older kernels as well :)

--- v4l2/generic.h
+++ v4l2/generic.h
@@ -22,9 +22,10 @@
 #include <linux/pci.h>
 #include <linux/videodev.h>
 #include <linux/version.h>
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,17)
 #include <linux/poll.h>
 #include <media/v4l2-dev.h>
-
+#endif
 #define GENERIC_VERSION_CODE KERNEL_VERSION(0,0,1)
 #define MAX_CARDS 10
 #ifdef dprintk
